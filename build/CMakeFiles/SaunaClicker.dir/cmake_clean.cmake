b5d776d9c86a9b869df59a1341780f4742154426
eerojurg <eerojurg@ut.ee>
2024-05-26 21:36:29 +0300

    Pushing mäng

---
+++ build/CMakeFiles/SaunaClicker.dir/cmake_clean.cmake
@@ -0,0 +1,13 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SaunaClicker.dir/sauna_clicker/main.cpp.o"
+  "CMakeFiles/SaunaClicker.dir/sauna_clicker/main.cpp.o.d"
+  "CMakeFiles/SaunaClicker.dir/sauna_clicker/saunaclicker.cpp.o"
+  "CMakeFiles/SaunaClicker.dir/sauna_clicker/saunaclicker.cpp.o.d"
+  "SaunaClicker"
+  "SaunaClicker.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/SaunaClicker.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/CMakeFiles/SaunaClicker.dir/compiler_depend.make
@@ -0,0 +1,2 @@
+# Empty compiler generated dependencies file for SaunaClicker.
+# This may be replaced when dependencies are built.
