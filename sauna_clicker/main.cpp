59a98b7374463fbe7458a17d4a06987f44508ada
Lontloom <kasparaednik@gmail.com>
2024-05-26 19:52:21 +0300

    Kõik 4 itemit on nüüd siin ja Threadid on töökorras

--- sauna_clicker/main.cpp
+++ sauna_clicker/main.cpp
@@ -1,6 +1,6 @@
 #include <iostream>
 #include <SDL.h>
-#include <SDL_ttf.h>
+#include <SDL_ttf.h> // kontrollida kas arvutis on olemas SDL ja SDL_tff kataloogid. (selle .zip failiga peaks kõik kaasas olema(SDL2 KAUST, SDL2TTF KAUST ja siis DejaVuSans.tff)), lihtsalt vaja kontrollida failide kättesaamise pathingut
 #include <thread>
 #include "sauna.h"
 
@@ -8,7 +8,10 @@ using namespace std;
 
 Clicker automaatClicker;
 Clicker automaatClicker2;
+Clicker automaatClicker3;
+Clicker automaatClicker4;
 Clicker keriseRect;
+
 
 int main(int argc, char *argv[]) {
     //////////////////////////////////////////////////
@@ -43,14 +46,22 @@ int main(int argc, char *argv[]) {
 
     automaatClicker.rect = {60, 50, 375, 50}; // Vasakule üles kast (automaatklikeri jaoks)
     automaatClicker2.rect = {60, 100, 375, 50};
+    automaatClicker3.rect = {60, 150, 375, 50};
+    automaatClicker4.rect = {60, 200, 375, 50};
     automaatClicker.enabled = false;
     automaatClicker2.enabled = false;
+    automaatClicker3.enabled = false;
+    automaatClicker4.enabled = false;
     int kogus = 0;
     int kogus2 = 0;
+    int kogus3 = 0;
+    int kogus4 = 0;
     keriseRect.rect = {AKNA_LAIUS / 2 - 50, 380, 150, 300};
+    olukast.rect = {400, 400, 100, 100};
+    olukast.enabled = false;
+
 
     SDL_Rect poodRect = {10, 10, 80, 40}; // Vasakule üles kast (Poe jaoks kast)
-
 
     //////////////////////////////////////////////////
     //                Main Loop                     //
@@ -58,18 +69,25 @@ int main(int argc, char *argv[]) {
     bool välju = false;
     thread automaatneKlikkThread;
     thread automaatneKlikkThread2;
+    thread automaatneKlikkThread3;
+    thread automaatneKlikkThread4;
+    thread oluLoim(oluHullus);
     while (!välju) {
         SDL_Event sündmus;
         while (SDL_PollEvent(&sündmus)) {
             if (sündmus.type == SDL_QUIT) {
+                automaatneKlikkThread.detach();
+                automaatneKlikkThread2.detach();
+                automaatneKlikkThread3.detach();
+                automaatneKlikkThread4.detach();
                 välju = true;
             } else if (sündmus.type == SDL_MOUSEBUTTONDOWN) {
                 if (sündmus.button.button == SDL_BUTTON_LEFT) {
                     int hiirX, hiirY;
                     SDL_GetMouseState(&hiirX, &hiirY);
                     if (kursorClickeril(keriseRect, hiirX, hiirY)) {
-                        kraadid += 0.01f;
-                        saunaraha += 1;
+                        kraadid += klikimisväärtus;
+                        saunaraha += klikimisväärtus * 100;
                         cout << "Kraadid: " << fixed << setprecision(2) << kraadid << endl;
                     } else if (kursorClickeril(automaatClicker, hiirX, hiirY) && automaatClicker.enabled) {
                         ostaAutomaatClicker();
@@ -97,10 +115,42 @@ int main(int argc, char *argv[]) {
                             std::thread uusAutomaatneKlikkThread2(automaatneKlikk2); // Luua uus lõim
                             automaatneKlikkThread2 = std::move(uusAutomaatneKlikkThread2); // Kasuta uut lõime
                         }
+                    } else if (kursorClickeril(automaatClicker3, hiirX, hiirY) && automaatClicker3.enabled) {
+                        ostaAutomaatClicker3();
+                        kogus3++;
+                        if (automaatClickerOlemas3) {
+                            if (automaatneKlikkThread3.joinable()) {
+                                automaatneKlikkLubatud3 = false;
+                                automaatneKlikkThread3.join();
+                                automaatneKlikkLubatud3 = true;
+                            }
+
+                            std::thread uusAutomaatneKlikkThread3(automaatneKlikk3); // Luua uus lõim
+                            automaatneKlikkThread3 = std::move(uusAutomaatneKlikkThread3); // Kasuta uut lõime
+                        }
+                    } else if (kursorClickeril(automaatClicker4, hiirX, hiirY) && automaatClicker4.enabled) {
+                        ostaAutomaatClicker4();
+                        kogus4++;
+                        if (automaatClickerOlemas4) {
+                            if (automaatneKlikkThread4.joinable()) {
+                                automaatneKlikkLubatud4 = false;
+                                automaatneKlikkThread4.join();
+                                automaatneKlikkLubatud4 = true;
+                            }
+
+                            std::thread uusAutomaatneKlikkThread4(automaatneKlikk4); // Luua uus lõim
+                            automaatneKlikkThread4 = std::move(uusAutomaatneKlikkThread4); // Kasuta uut lõime
+                        }
+                    } else if (kursorClickeril(olukast, hiirX, hiirY)) {
+                        klikimisväärtus += 0.03f;
+                        saunaraha += 300;
+                        olukast.enabled = false;
                     }
                 }
             }
+
         }
+
 
         //////////////////////////////////////////////////
         //       SDL-iga joonistame teksti ja kaste     //
@@ -115,13 +165,38 @@ int main(int argc, char *argv[]) {
          */
 
         // Laen fonti
-        TTF_Font *font = TTF_OpenFont("C:/Users/Kaspar/CLionProjects/SaunaClicker/DejaVuSans.ttf", 24);
+        TTF_Font *font = TTF_OpenFont("DejaVuSans.ttf",
+                                      24); //siin on vaja korigeerida, kus arvutis asub DejaVuSans.tff (.zip failis on see olemas SaunaClicker/DejaVuSans.tff)
         if (!font) {
-            cerr << "Fonti ei toimi: " << TTF_GetError() << endl;
+            cerr << "Font ei toimi: " << TTF_GetError() << endl;
             TTF_Quit();
             SDL_Quit();
             exit(1);
         }
+
+        if (olukast.enabled) {
+            SDL_SetRenderDrawColor(renderdaja, 255, 255, 0, 255);
+            SDL_RenderFillRect(renderdaja, &olukast.rect);
+            renderText(font, "Olu", {0, 0, 0, 255}, olukast.rect.x +20,
+                       olukast.rect.y + 50);
+
+            SDL_Event sündmus;
+            while (SDL_PollEvent(&sündmus)) {
+                if (sündmus.type == SDL_MOUSEBUTTONDOWN) {
+                    if (sündmus.button.button == SDL_BUTTON_LEFT) {
+                        int hiirX, hiirY;
+                        SDL_GetMouseState(&hiirX, &hiirY);
+                        if (kursorClickeril(olukast, hiirX, hiirY)) {
+                            kraadid += 0.5f;
+                        }
+                    }
+                }
+            }
+            thread kraadideLoim(ajutineKraadid);
+            kraadideLoim.detach();
+        }
+
+
 
         // Renderdan tekste
         if (automaatClicker.enabled) {
@@ -129,11 +204,11 @@ int main(int argc, char *argv[]) {
             kogutekst << kogus << "x";
             SDL_SetRenderDrawColor(renderdaja, 0, 255, 0, 255); // Roheline värv
             SDL_RenderFillRect(renderdaja, &automaatClicker.rect);
-            renderText(font, "Vee tilgutaja (+0.01 kraadi/s)", {65, 107, 223, 255}, automaatClicker.rect.x + 10,
+            renderText(font, "Vee tilgutaja (+0.02 kraadi/s)", {65, 107, 223, 255}, automaatClicker.rect.x + 10,
                        automaatClicker.rect.y + 10);
-            if(kogus > 0)
-            renderText(font, kogutekst.str(), {255, 223, 0, 255}, automaatClicker.rect.x - 35,
-                       automaatClicker.rect.y + 20);
+            if (kogus > 0)
+                renderText(font, kogutekst.str(), {255, 223, 0, 255}, automaatClicker.rect.x - 35,
+                           automaatClicker.rect.y + 20);
         }
 
         if (automaatClicker2.enabled) {
@@ -141,11 +216,37 @@ int main(int argc, char *argv[]) {
             kogutekst2 << kogus2 << "x";
             SDL_SetRenderDrawColor(renderdaja, 0, 255, 0, 255); // Roheline värv
             SDL_RenderFillRect(renderdaja, &automaatClicker2.rect);
-            renderText(font, "Kuumkivi (+0.05 kraadi/s)", {65, 107, 223, 255}, automaatClicker2.rect.x + 10,
+            renderText(font, "Kuumkivi (+0.08 kraadi/s)", {65, 107, 223, 255}, automaatClicker2.rect.x + 10,
                        automaatClicker2.rect.y + 10);
-            if(kogus2 > 0)
-            renderText(font, kogutekst2.str(), {255, 223, 0, 255}, automaatClicker2.rect.x - 35,
-                       automaatClicker2.rect.y + 20);
+            if (kogus2 > 0)
+                renderText(font, kogutekst2.str(), {255, 223, 0, 255}, automaatClicker2.rect.x - 35,
+                           automaatClicker2.rect.y + 20);
+
+        }
+
+        if (automaatClicker3.enabled) {
+            stringstream kogutekst3;
+            kogutekst3 << kogus3 << "x";
+            SDL_SetRenderDrawColor(renderdaja, 0, 255, 0, 255); // Roheline värv
+            SDL_RenderFillRect(renderdaja, &automaatClicker3.rect);
+            renderText(font, "Syytetabletid (+0.2 kraadi/s)", {65, 107, 223, 255}, automaatClicker3.rect.x + 10,
+                       automaatClicker3.rect.y + 10);
+            if (kogus3 > 0)
+                renderText(font, kogutekst3.str(), {255, 223, 0, 255}, automaatClicker3.rect.x - 35,
+                           automaatClicker3.rect.y + 20);
+
+        }
+
+        if (automaatClicker4.enabled) {
+            stringstream kogutekst4;
+            kogutekst4 << kogus4 << "x";
+            SDL_SetRenderDrawColor(renderdaja, 0, 255, 0, 255); // Roheline värv
+            SDL_RenderFillRect(renderdaja, &automaatClicker4.rect);
+            renderText(font, "Pelletimasin (+1 kraad/s)", {65, 107, 223, 255}, automaatClicker4.rect.x + 10,
+                       automaatClicker4.rect.y + 10);
+            if (kogus4 > 0)
+                renderText(font, kogutekst4.str(), {255, 223, 0, 255}, automaatClicker4.rect.x - 35,
+                           automaatClicker4.rect.y + 20);
 
         }
 
@@ -153,6 +254,7 @@ int main(int argc, char *argv[]) {
         SDL_SetRenderDrawColor(renderdaja, 255, 255, 255, 255); // Valge värv
         SDL_RenderFillRect(renderdaja, &poodRect);
         renderText(font, "Pood:", {0, 0, 0, 255}, poodRect.x + 5, poodRect.y + 5);
+
 
         // Näita kraade ringi all
         stringstream skooriTekst;
@@ -173,14 +275,13 @@ int main(int argc, char *argv[]) {
         // Kontrollige, kas automaatClicker ostuaken peaks olema aktiivne
         automaatClicker.enabled = saunaraha >= automaatclickerihind;
         automaatClicker2.enabled = saunaraha >= automaatclickerihind2;
+        automaatClicker3.enabled = saunaraha >= automaatclickerihind3;
+        automaatClicker4.enabled = saunaraha >= automaatclickerihind4;
     }
 
     //////////////////////////////////////////////////
     //                 Cleanup                      //
     //////////////////////////////////////////////////
-    if (automaatneKlikkThread.joinable()) {
-        automaatneKlikkThread.join();
-    }
 
     TTF_Quit();
     SDL_DestroyRenderer(renderdaja);
--- sauna_clicker/sauna.h
+++ sauna_clicker/sauna.h
@@ -7,6 +7,7 @@
 #include <SDL_ttf.h>
 #include <chrono>
 #include <thread>
+#include <random>
 
 using namespace std;
 
@@ -22,14 +23,24 @@ const float AUTOMAAT_CLICKER_KLIKIAEG_SEK = 0.02f;
 //////////////////////////////////////////////////
 extern SDL_Renderer* renderdaja;
 extern float kraadid;
+extern float klikimisväärtus;
 extern int saunaraha;
 extern bool automaatClickerOlemas;
 extern bool automaatneKlikkLubatud;
 extern int automaatclickerihind;
+
 extern bool automaatClickerOlemas2;
 extern bool automaatneKlikkLubatud2;
 extern int automaatclickerihind2;
 
+extern bool automaatClickerOlemas3;
+extern bool automaatneKlikkLubatud3;
+extern int automaatclickerihind3;
+
+extern bool automaatClickerOlemas4;
+extern bool automaatneKlikkLubatud4;
+extern int automaatclickerihind4;
+extern bool kraadideLisamine;
 //////////////////////////////////////////////////
 //               Kliki struktuur                //
 //////////////////////////////////////////////////
@@ -38,12 +49,20 @@ struct Clicker {
     bool enabled;
 };
 
+extern Clicker olukast;
+
 void ring(SDL_Renderer* renderdaja, int x, int y, int raadius);
 void ostaAutomaatClicker();
 void ostaAutomaatClicker2();
+void ostaAutomaatClicker3();
+void ostaAutomaatClicker4();
 bool kursorClickeril(Clicker clicker, int x, int y);
 void automaatneKlikk();
 void automaatneKlikk2();
+void automaatneKlikk3();
+void automaatneKlikk4();
 void renderText(TTF_Font* font, const string& text, const SDL_Color& color, int x, int y);
+void oluHullus();
+void ajutineKraadid();
 
 #endif
